42778b7cf3a7c238909f415dc56d7b4d37360846
Papp Dániel <pappdani2@gmail.com>
2023-11-21 10:15:53 +0100

    merged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Signup from './components/Signup'
 import Login from './components/Login'
 import Home from './components/Home'
+import Admin from './components/Admin'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
+      <Admin />
     </div>
   )
 }
